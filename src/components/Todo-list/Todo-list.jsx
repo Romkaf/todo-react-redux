c5999f4b07412adba94b1bf4e10a0e8731e04045
Roman <roman@MacBook-Pro-Roman.local>
2020-10-28 19:41:01 +0300

    Change the array of todos in TodoList when changing the filter

--- src/components/Todo-list/Todo-list.jsx
+++ src/components/Todo-list/Todo-list.jsx
@@ -4,9 +4,21 @@ import { connect } from 'react-redux';
 import { deleteTodo, selectTodo, editTodo } from '@models/actions';
 import PropTypes from 'prop-types';
 
-const TodoList = ({ todosArray, selectTodo, deleteTodo, editTodo }) => {
-	console.log(todosArray);
-	const todos = todosArray.map((it) => {
+const TodoList = ({ todosArray, filter, selectTodo, deleteTodo, editTodo }) => {
+	const filterTodos = (todos, filter) => {
+		switch (filter) {
+			case 'all':
+				return todos;
+			case 'active':
+				return todos.filter((it) => !it.completed);
+			case 'completed':
+				return todos.filter((it) => it.completed);
+			default:
+				return todos;
+		}
+	};
+	const visibleTodos = filterTodos(todosArray, filter);
+	const todos = visibleTodos.map((it) => {
 		return (
 			<li key={it.id}>
 				<TodoItem
@@ -23,8 +35,9 @@ const TodoList = ({ todosArray, selectTodo, deleteTodo, editTodo }) => {
 	return <ul className="todo-list">{todos}</ul>;
 };
 
-const mapStateToProps = ({ todosArray }) => ({
+const mapStateToProps = ({ todosArray, filter }) => ({
 	todosArray,
+	filter,
 });
 
 const mapDispatchToProps = {
@@ -37,6 +50,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
 
 TodoList.propTypes = {
 	todosArray: PropTypes.arrayOf(PropTypes.object),
+	filter: PropTypes.string,
 	deleteTodo: PropTypes.func,
 	editTodo: PropTypes.func,
 	selectTodo: PropTypes.func,
